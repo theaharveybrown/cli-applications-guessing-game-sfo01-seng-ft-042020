066722ba843dd10c4f5c46bb2a988849ced47194
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-14 18:25:25 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
   random_num = rand(6) + 1
   puts "Guess a number between 1 and 6."
   input = gets.chomp
-  if input == randome_num
+  if input == random_num
     puts "random num was #{random_num}"
     return "YOU WIN!"
   else 
